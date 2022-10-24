e24da4ba3cf9279763287fceeb93cf1252cd3314
Teayyyy <zgsighoty@outlook.com>
2022-10-24 22:10:12 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 欧天逸 on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
